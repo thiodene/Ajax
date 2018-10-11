1cdade8dde1edb144ec94467fbc53d668b396ec3
Serge Ayissi <ayissi.serge@gmail.com>
2018-10-11 11:23:02 -0400

    Create trigger_ajax_one_time.js

---
+++ replace_html_one_time/trigger_ajax_one_time.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("#display").click(function() {                
+    //alert("OK") ;
+    equipment = $(this).attr("equipment") ;
+    //alert(equipment) ;
+    $.ajax({    //create an ajax request to display.php
+      type: "GET",
+      url: "/includes/php/tests/display.php?equipment=" + equipment,             
+      dataType: "html",   //expect html to be returned                
+      success: function(response)
+      {                    
+        $("#responsecontainer").html(response); 
+        //alert(response);
+      }
+
+    });
+  });
+});
